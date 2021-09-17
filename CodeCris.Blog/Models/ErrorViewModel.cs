b46ccf6e6b92034d13a96d6bc043715719fdd61e
kchowanski <kc@oddslife.com>
2021-09-17 14:23:17 +0200

    init

---
+++ CodeCris.Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeCris.Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
